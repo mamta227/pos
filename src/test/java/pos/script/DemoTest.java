16f7a80d08e77cd6ae6dc9447fe7fd96795a169c
mamat <mamat@NU14A1.mshome.net>
2023-07-06 07:49:38 +0530

    pos

---
+++ src/test/java/pos/script/DemoTest.java
@@ -0,0 +1,15 @@
+package pos.script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class DemoTest extends BaseTest {
+	@Test
+	public void testA()
+	{
+		Reporter.log("testA....", true);
+	}
+
+}
